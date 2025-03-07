5975ee2423445bd5dd3c5cc0e1030274a4cd63dd
ideal-jiwon <mok.ji@northeastern.edu>
2025-03-07 17:40:20 -0500

    3

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-
-# 패키지 설치
-pip install -r requirements.txt
-
-# Gunicorn 실행 (Flask 서버)
-exec gunicorn -w 4 -b 0.0.0.0:8000 server:app
+echo "#!/bin/bash" > startup.sh
+echo "gunicorn -w 4 -b 0.0.0.0:8000 server:app" >> startup.sh
+chmod +x startup.sh
 
