ee0f57c90473499b6ae1406491b9b12df95d0bed
Stellar Ji won Mok <mok.ji@northeastern.edu>
2025-05-11 19:56:03 -0400

    🔥 Remove startup.sh to fix Azure startup

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://restaurant-ai-app.azurewebsites.net";
+const BACKEND_URL = "solutionpitch-h4dzfecubpa4b6gc.eastus-01.azurewebsites.net";
 
 
 fetch(`${BACKEND_URL}/models/realtime_sentiment?business_id=test`)
